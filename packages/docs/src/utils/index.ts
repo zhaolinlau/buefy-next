34bb44c0bf171229e4e95c48946b7f8402cc0822
Kikuo Emoto <kemoto@codemonger.io>
2024-01-22 13:04:14 +0900

    feat(docs): rewrite utils/index in TS
    - Rewrites `utils/index.js` → `utils/index.ts` in TypeScript. Gives
      types to `preformat` function.
    
      `shallowFields` is not typed yet, which was introduced by `rebase`.
      I decided not to type it at this commit.

--- packages/docs/src/utils/index.ts
+++ packages/docs/src/utils/index.ts
@@ -1,22 +1,22 @@
 import { shallowRef } from 'vue'
+import type { ShallowRef } from 'vue'
 
-export function preformat(text) {
+export function preformat(text?: string): string | undefined {
     if (!text) return
 
     // Remove first blank line
     text = text.replace(/^\s*[\r\n]/g, '')
 
     // Find how many whitespaces before the first character of the first line
+    // @ts-expect-error - the pattern should always match
     const whitespaces = /^[ \t]*./.exec(text).toString().slice(0, -1)
 
     // Replace first occurrance of whitespace on each line
-    let newText = []
+    const newText: string[] = []
     text.split(/\r\n|\r|\n/).forEach((line) => {
         newText.push(line.replace(whitespaces, ''))
     })
-    newText = newText.join('\r\n')
-
-    return newText
+    return newText.join('\r\n')
 }
 
 export function shallowFields(fields) {
