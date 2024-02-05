d73ada3fde5599c934be6af5475a7c4592091720
Kikuo Emoto <kemoto@codemonger.io>
2024-02-05 13:00:50 +0900

    test(lib): rewrite test for CheckRadioMixin in TS
    - Rewrites the test for `CheckRadioMixin` in TypeScript, which is
      defined in `utils/CheckRadioMixin.spec`:
        - Replaces the extension: `.js` → `.ts`
        - Imports necessary methods and object from `vitest`
        - Replaces `jest.fn` with `vi.fn`
        - Wraps `component` definition with `defineComponent` API to
          properly type `wrapper`

--- packages/buefy-next/src/utils/CheckRadioMixin.spec.ts
+++ packages/buefy-next/src/utils/CheckRadioMixin.spec.ts
@@ -1,14 +1,17 @@
+import { defineComponent } from 'vue'
 import { shallowMount } from '@vue/test-utils'
+import type { VueWrapper } from '@vue/test-utils'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import CheckRadioMixin from '@utils/CheckRadioMixin'
 
 describe('CheckRadioMixin', () => {
-    HTMLElement.prototype.insertAdjacentElement = jest.fn()
+    HTMLElement.prototype.insertAdjacentElement = vi.fn()
 
-    const component = {
-        template: '<div class="b-component"></div>',
-        mixins: [CheckRadioMixin]
-    }
-    let wrapper
+    const component = defineComponent({
+        mixins: [CheckRadioMixin],
+        template: '<div class="b-component"></div>'
+    })
+    let wrapper: VueWrapper<InstanceType<typeof component>>
 
     beforeEach(() => {
         wrapper = shallowMount(component, {
