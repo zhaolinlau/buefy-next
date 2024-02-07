f82989de6d9a1ba393af95fb76d337c6910de983
Kikuo Emoto <kemoto@codemonger.io>
2024-02-07 11:13:22 +0900

    test(lib): rewrite FormElementMixin.spec in TS
    - Rewrites `utils/FormElementMixin.spec.js` in TypeScript:
        - Replaces the extension: `.js` → `.ts`
        - Imports necessary methods and object from `vitest`
        - Replaces `jest.fn` with `vi.fn`
        - Types `component` with `defineComponent` API
        - Types `wrapper`
        - Adds `FocusEvent` arguments to `onFocus` and `onBlur`

--- packages/buefy-next/src/utils/FormElementMixin.spec.ts
+++ packages/buefy-next/src/utils/FormElementMixin.spec.ts
@@ -1,14 +1,17 @@
+import { defineComponent } from 'vue'
 import { shallowMount } from '@vue/test-utils'
+import type { VueWrapper } from '@vue/test-utils'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import FormElementMixin from '@utils/FormElementMixin'
 
 describe('FormElementMixin', () => {
-    HTMLElement.prototype.insertAdjacentElement = jest.fn()
+    HTMLElement.prototype.insertAdjacentElement = vi.fn()
 
-    const component = {
-        template: '<div class="b-component"></div>',
-        mixins: [FormElementMixin]
-    }
-    let wrapper
+    const component = defineComponent({
+        mixins: [FormElementMixin],
+        template: '<div class="b-component"></div>'
+    })
+    let wrapper: VueWrapper<InstanceType<typeof component>>
 
     beforeEach(() => {
         wrapper = shallowMount(component, {
@@ -21,15 +24,15 @@ describe('FormElementMixin', () => {
     })
 
     it('should set isFocused and emit focus event on onFocus', async () => {
-        wrapper.vm.onFocus()
+        wrapper.vm.onFocus(new FocusEvent('focus'))
         await wrapper.vm.$nextTick()
         expect(wrapper.vm.isFocused).toBeTruthy()
         expect(wrapper.emitted().focus).toBeTruthy()
     })
 
     it('should reset isFocused and emit blur event on onBlur', async () => {
-        wrapper.vm.checkHtml5Validity = jest.fn()
-        wrapper.vm.onBlur()
+        wrapper.vm.checkHtml5Validity = vi.fn()
+        wrapper.vm.onBlur(new FocusEvent('blur'))
         await wrapper.vm.$nextTick()
         expect(wrapper.vm.isFocused).toBeFalsy()
         expect(wrapper.emitted().blur).toBeTruthy()
