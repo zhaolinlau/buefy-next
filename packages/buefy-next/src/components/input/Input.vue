68a8d7e4a479bdd129c1dfaf10368bb7d6e952a4
Kikuo Emoto <kemoto@codemonger.io>
2024-02-07 12:18:35 +0900

    feat(lib): rewrite Input in TS
    - Rewrites `components/input/Input.vue` in TS:
        - Adds `lang="ts"` to the script section
        - Wraps the entire component definition with `defineComponent` API
        - Types emits, methods, and computed value setter
        - Statically registers `BIcon` so that type checking works:
          `[Icon.name]: Icon` → `BIcon`
        - Gives a specific type to `modelValue` prop so that it accepts
          `null`
        - `onInput` and `onChange` event handlers assume the `event.target`
          is `HTMLInputElement` or `HTMLTextAreaElement`
        - Removes double asterisks in comments to prevent `api-extractor`
          from picking them up for document generation
    
      The following changes might not be trivial:
        - `b-icon` for the right icon is not rendered if `rightIcon` is
          `undefined`. Because `icon` prop of `b-icon` should not be
          omitted.
        - `rightIconType` computed type returns `undefined` instead of
          `null` when `iconRightType` is not set. Because `type` prop of
          `b-icon` does not accept `null`.
        - Converts `computedValue` into `undefined` if it is `null` because
          `value` attribute of `textarea` does not accept `null` according
          to the Vue definition
    
    - Rewrites `components/input/index.js` in TS:
        - Replaces the extension: `.js` → `.ts`
        - Types the plugin `install` method

--- packages/buefy-next/src/components/input/Input.vue
+++ packages/buefy-next/src/components/input/Input.vue
@@ -25,7 +25,7 @@
             class="textarea"
             :class="[inputClasses, customClass]"
             :maxlength="maxlength"
-            :value="computedValue"
+            :value="computedValue === null ? undefined : computedValue"
             v-bind="$attrs"
             @input="onInput"
             @change="onChange"
@@ -44,7 +44,7 @@
         />
 
         <b-icon
-            v-if="!loading && hasIconRight"
+            v-if="!loading && hasIconRight && rightIcon"
             class="is-right"
             :class="{ 'is-clickable': passwordReveal || iconRightClickable }"
             :icon="rightIcon"
@@ -65,20 +65,23 @@
     </div>
 </template>
 
-<script>
-import Icon from '../icon/Icon.vue'
+<script lang="ts">
+import { defineComponent } from 'vue'
+import type { PropType } from 'vue'
+
+import BIcon from '../icon/Icon.vue'
 import config from '../../utils/config'
 import FormElementMixin from '../../utils/FormElementMixin'
 
-export default {
+export default defineComponent({
     name: 'BInput',
-    components: {
-        [Icon.name]: Icon
-    },
+    components: { BIcon },
     mixins: [FormElementMixin],
     inheritAttrs: false,
     props: {
-        modelValue: [Number, String],
+        modelValue: {
+            type: [Number, String] as PropType<string | number | undefined | null>
+        },
         type: {
             type: String,
             default: 'text'
@@ -101,11 +104,13 @@ export default {
         iconRightClickable: Boolean,
         iconRightType: String
     },
-    emits: [
-        'icon-click',
-        'icon-right-click',
-        'update:modelValue'
-    ],
+    emits: {
+        /* eslint-disable @typescript-eslint/no-unused-vars */
+        'icon-click': (event: MouseEvent) => true,
+        'icon-right-click': (event: MouseEvent) => true,
+        'update:modelValue': (value: string | number | undefined) => true
+        /* eslint-enable @typescript-eslint/no-unused-vars */
+    },
     data() {
         return {
             newValue: this.modelValue,
@@ -122,7 +127,7 @@ export default {
             get() {
                 return this.newValue
             },
-            set(value) {
+            set(value: string | number | undefined) {
                 this.newValue = value
                 this.$emit('update:modelValue', value)
             }
@@ -161,7 +166,7 @@ export default {
             if (this.passwordReveal) {
                 return 'is-primary'
             } else if (this.iconRight) {
-                return this.iconRightType || null
+                return this.iconRightType || undefined
             }
             return this.statusType
         },
@@ -183,7 +188,7 @@ export default {
             return iconClasses
         },
 
-        /**
+        /*
         * Icon name (MDI) based on the type.
         */
         statusTypeIcon() {
@@ -196,20 +201,20 @@ export default {
             }
         },
 
-        /**
+        /*
         * Check if have any message prop from parent if it's a Field.
         */
         hasMessage() {
             return !!this.statusMessage
         },
 
-        /**
+        /*
         * Current password-reveal icon name.
         */
         passwordVisibleIcon() {
             return !this.isPasswordVisible ? 'eye' : 'eye-off'
         },
-        /**
+        /*
         * Get value length
         */
         valueLength() {
@@ -222,7 +227,7 @@ export default {
         }
     },
     watch: {
-        /**
+        /*
         * When v-model is changed:
         *   1. Set internal value.
         */
@@ -234,7 +239,7 @@ export default {
         }
     },
     methods: {
-        /**
+        /*
         * Toggle the visibility of a password-reveal input
         * by changing the type and focus the input right away.
         */
@@ -247,14 +252,17 @@ export default {
             })
         },
 
-        iconClick(emit, event) {
-            this.$emit(emit, event)
+        iconClick(emit: 'icon-click' | 'icon-right-click', event: MouseEvent) {
+            // $emit is overloaded for each of icon-click and icon-right-click
+            // but not for both icon-click | icon-right-click
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            this.$emit(emit as any, event)
             this.$nextTick(() => {
                 this.focus()
             })
         },
 
-        rightIconClick(event) {
+        rightIconClick(event: MouseEvent) {
             if (this.passwordReveal) {
                 this.togglePasswordVisibility()
             } else if (this.iconRightClickable) {
@@ -262,24 +270,24 @@ export default {
             }
         },
 
-        onInput(event) {
+        onInput(event: Event) {
             if (!this.lazy) {
-                const value = event.target.value
+                const value = (event.target as HTMLInputElement | HTMLTextAreaElement).value
                 this.updateValue(value)
             }
         },
 
-        onChange(event) {
+        onChange(event: Event) {
             if (this.lazy) {
-                const value = event.target.value
+                const value = (event.target as HTMLInputElement | HTMLTextAreaElement).value
                 this.updateValue(value)
             }
         },
 
-        updateValue(value) {
+        updateValue(value: string | number | undefined) {
             this.computedValue = value
             !this.isValid && this.checkHtml5Validity()
         }
     }
-}
+})
 </script>
