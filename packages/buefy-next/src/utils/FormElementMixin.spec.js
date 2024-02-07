6ff20b3a67fd342c9ce4484445c364c703923401
Kikuo Emoto <kemoto@codemonger.io>
2024-02-07 11:05:11 +0900

    test(lib): prepare FormElementMixin.spec for TS
    - Prepares `utils/FormElementMigration.spec.js` for TypeScript
      migration:
        - Replaces the legacy async tests (`done`) with async functions
        - Moves `component` and `wrapper` so that `wrapper` can be properly
          typed

--- packages/buefy-next/src/utils/FormElementMixin.spec.js
+++ packages/buefy-next/src/utils/FormElementMixin.spec.js
@@ -1,15 +1,16 @@
 import { shallowMount } from '@vue/test-utils'
 import FormElementMixin from '@utils/FormElementMixin'
 
-let wrapper
-
 describe('FormElementMixin', () => {
     HTMLElement.prototype.insertAdjacentElement = jest.fn()
+
+    const component = {
+        template: '<div class="b-component"></div>',
+        mixins: [FormElementMixin]
+    }
+    let wrapper
+
     beforeEach(() => {
-        const component = {
-            template: '<div class="b-component"></div>',
-            mixins: [FormElementMixin]
-        }
         wrapper = shallowMount(component, {
             attachTo: document.body
         })
@@ -19,23 +20,19 @@ describe('FormElementMixin', () => {
         expect(wrapper.vm.isValid).toBeTruthy()
     })
 
-    it('should set isFocused and emit focus event on onFocus', (done) => {
+    it('should set isFocused and emit focus event on onFocus', async () => {
         wrapper.vm.onFocus()
-        wrapper.vm.$nextTick(() => {
-            expect(wrapper.vm.isFocused).toBeTruthy()
-            expect(wrapper.emitted().focus).toBeTruthy()
-            done()
-        })
+        await wrapper.vm.$nextTick()
+        expect(wrapper.vm.isFocused).toBeTruthy()
+        expect(wrapper.emitted().focus).toBeTruthy()
     })
 
-    it('should reset isFocused and emit blur event on onBlur', (done) => {
+    it('should reset isFocused and emit blur event on onBlur', async () => {
         wrapper.vm.checkHtml5Validity = jest.fn()
         wrapper.vm.onBlur()
-        wrapper.vm.$nextTick(() => {
-            expect(wrapper.vm.isFocused).toBeFalsy()
-            expect(wrapper.emitted().blur).toBeTruthy()
-            expect(wrapper.vm.checkHtml5Validity).toHaveBeenCalled()
-            done()
-        })
+        await wrapper.vm.$nextTick()
+        expect(wrapper.vm.isFocused).toBeFalsy()
+        expect(wrapper.emitted().blur).toBeTruthy()
+        expect(wrapper.vm.checkHtml5Validity).toHaveBeenCalled()
     })
 })
