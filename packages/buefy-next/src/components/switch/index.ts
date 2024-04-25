182d47a562a1505a7516a071af71e603c5fc5555
Wesley Ford <wes@wesdev.pro>
2024-04-25 17:30:52 -0500

    Rewrite Switch in TS (ntohq/buefy-next#237)
    * feat(lib): rewrite Switch in TS
    
    - Rewrites `Switch .vue` in TypeScript:
      - Introduces a new type `SwitchProps` that extracts the props type
        of `Switch `.
    
      Trivial changes:
      - Adds `lang="ts"` in the script section
      - Wraps the entire component definition in the `defineComponent` API
      - Gives minimal types
    
    - Rewrites `components/switch/index.js` in TypeScript
    
     Trivial changes for the Switch Component:
      - Replaces the extension: ".js" → ".ts"
      - Gives minimal types

--- packages/buefy-next/src/components/switch/index.ts
+++ packages/buefy-next/src/components/switch/index.ts
@@ -1,9 +1,9 @@
+import type { App } from 'vue'
 import Switch from './Switch.vue'
-
 import { use, registerComponent } from '../../utils/plugins'
 
 const Plugin = {
-    install(Vue) {
+    install(Vue: App) {
         registerComponent(Vue, Switch)
     }
 }
