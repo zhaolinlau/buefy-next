bac18308d71be0922062afc38a4836d8b0b2f3e8
Wesley Ford <wes@wesdevpro.com>
2024-08-26 09:05:32 -0400

    test(lib): rewrite Upload.spec in TS

--- packages/buefy-next/src/components/upload/Upload.spec.ts
+++ packages/buefy-next/src/components/upload/Upload.spec.ts
@@ -1,7 +1,9 @@
 import { shallowMount } from '@vue/test-utils'
+import type { VueWrapper } from '@vue/test-utils'
+import { beforeEach, describe, expect, it } from 'vitest'
 import BUpload from '@components/upload/Upload.vue'
 
-let wrapper
+let wrapper: VueWrapper<InstanceType<typeof BUpload>>
 
 describe('BUpload', () => {
     beforeEach(() => {
